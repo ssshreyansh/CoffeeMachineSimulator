f75d517a8fd854d54f6a29190db1e982ab28900b
ssshreyansh <166708527+ssshreyansh@users.noreply.github.com>
2024-09-25 16:26:50 +0400

    Add files via upload

---
+++ lab2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define espresso_beans 8
+#define espresso_water 30
+#define espresso_milk 0
+#define espresso_chocolate 0
+#define espresso_price 3.5
+
+#define cappuccino_beans 8
+#define cappuccino_water 30
+#define cappuccino_milk 70
+#define cappuccino_chocolate 0
+#define cappuccino_price 4.5
+
+#define mocha_beans 8
+#define mocha_water 39
+#define mocha_milk 160
+#define mocha_chocolate 30
+#define mocha_price 5.5 
